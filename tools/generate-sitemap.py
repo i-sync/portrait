b5d1758a73b31e3e941527f0a8ec783561da4a13
michael <michael@qq.com>
2022-09-25 19:22:36 +0800

    rollback sitemap.

--- tools/generate-sitemap.py
+++ tools/generate-sitemap.py
@@ -2,16 +2,12 @@
 import os
 import sys
 
-
-from sqlalchemy import func, desc
-
 sys.path.append('../')
 from app.library.config import configs
-from app.library.page import Page
 from app.library.models import session_scope, XiurenAlbum, XiurenCategory
 
 
-def generate_sitemap_category():
+def generate_sitemap():
     sitemap = f'''<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
@@ -32,24 +28,9 @@ def generate_sitemap_category():
     </url>'''
             sitemap += item
 
-    sitemap += '''
-</urlset>'''
-
-    with open("../sitemap-category.xml", "w+", encoding="utf-8") as f:
-        f.writelines(sitemap)
-
-
-def generate_sitemap_detail(offset, limit):
-
-    sitemap = f'''<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'''
-
     # article detail page
     with session_scope() as session:
-        albums = session.query(XiurenAlbum).filter(XiurenAlbum.is_enabled == 1).limit(limit).offset(offset).all()
-
-        categories = session.query(XiurenCategory).filter(XiurenCategory.is_enabled == 1).all()
-
+        albums = session.query(XiurenAlbum).filter(XiurenAlbum.is_enabled == 1).all()
         for album in albums:
             category = next((x for x in categories if x.id == album.category_id), None)
             item = f'''
@@ -59,42 +40,11 @@ def generate_sitemap_detail(offset, limit):
         <priority>0.5</priority>
     </url>'''
             sitemap += item
-
     sitemap += '''
 </urlset>'''
 
-    file_name = f"sitemap-{offset}-{limit}.xml"
-    with open(f"../{file_name}", "w+", encoding="utf-8") as f:
+    with open("../sitemap.xml", "w+", encoding="utf-8") as f:
         f.writelines(sitemap)
-
-    return file_name
 
 if __name__=="__main__":
-    generate_sitemap_category()
-
-    sitemap = """<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <sitemap>
-        <loc>https://portrait.knit.bid/sitemap-category.xml</loc>
-    </sitemap>
-"""
-    rows = 0
-    with session_scope() as session:
-        rows = session.query(func.count(XiurenAlbum.id)).filter(XiurenAlbum.is_enabled == 1).scalar()
-
-    page = Page(rows, page_index=1, page_size=500)
-    for i in range(1, page.page_count + 1):
-
-        page = Page(rows, page_index=i, page_size=500)
-        file_name = generate_sitemap_detail(page.offset, page.limit)
-        sitemap += f"""
-    <sitemap>
-        <loc>https://portrait.knit.bid/{file_name}</loc>
-    </sitemap>
-"""
-
-    sitemap += """
-</sitemapindex>"""
-
-    with open(f"../sitemap-index.xml", "w+", encoding="utf-8") as f:
-        f.writelines(sitemap)
-    print("Done")
+    generate_sitemap()
